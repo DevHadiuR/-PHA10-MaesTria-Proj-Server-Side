e1e9ea31dc14c218b29891afed908f9b825d62c3
Hadiur Rahman <hadiurahman139@gmail.com>
2024-04-29 09:25:43 +0600

    impliment get method to get data wiht datas id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -33,6 +33,13 @@ async function run() {
 
     app.get("/addedSculptures", async (req, res) => {
       const result = await sculptureCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/addedSculptures/:id", async (req, res) => {
+      const sculptureId = req.params.id;
+      const query = { _id: new ObjectId(sculptureId) };
+      const result = await sculptureCollection.findOne(query);
       res.send(result);
     });
 
